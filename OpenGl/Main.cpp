48f077af2d2fe1fb2e46410c5ed1916e1b79201a
Alex Gama <ssjminecrafter@outlook.com>
2022-09-27 16:05:43 +0100

    End of class commit

--- OpenGl/Main.cpp
+++ OpenGl/Main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <SDL.h>
 
+SDL_Window* window{ nullptr };
+SDL_GLContext context{ nullptr };
+
 int main(int argc, char* argv[])
 {
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) == -1)
+	{
+		std::cout << "SDL did not initialize properly." << std::endl;
+		return 0;
+	}
 
+	//set OpenGL context and profile using values assigned earlier
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+
+	window = SDL_CreateWindow("OpenGL Project", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+	if (!window)
+	{
+		std::cout << "OpenGL Window was not created properly." << std::endl;
+		return 0;
+	}
 
 	system("pause");
 	return 0;
